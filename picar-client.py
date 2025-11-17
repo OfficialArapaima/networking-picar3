ba6d50cab75df61310c57522143311b3761d9fb2
Arapaima <reesevz@gmail.com>
2025-11-17 10:38:49 -0600

    Got the server and the client working together repeatedly.

--- picar-client.py
+++ picar-client.py
@@ -1,5 +1,6 @@
 from socket import *
 
+
 manual = '''
 Press keys on keyboard to control PiCar-X!
     w: Forward
@@ -12,7 +13,8 @@ Press keys on keyboard to control PiCar-X!
 def show_info():
     print(manual)
 
-def client_connection():
+
+def main():
     serverName = input('Input the PiCar\'s server address:')
 
     serverPort = 12000
@@ -20,17 +22,13 @@ def client_connection():
     clientSocket.connect((serverName, serverPort))
 
 
-def main():
     while True:
         key = input('Input lowercase sentence:')
-        if key in ('wsad'):           
+        if key in ('wsad'):
             clientSocket.send(key.encode())
             modifiedSentence = clientSocket.recv(1024)
             print('From Server: ', modifiedSentence.decode())
-
-def close_connection():
+            
     clientSocket.close()
 
-client_connection()
 main()
-close_connection()
--- picar-server.py
+++ picar-server.py
@@ -1,14 +1,27 @@
+import sys
 from socket import *
 serverPort = 12000
+
+# bind the server to the socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(("", serverPort))
-serverSocket.listen(1024)
-print('The server is ready to receive')
+print("Server started on port: %s" % serverPort)
+serverSocket.listen(1)
+print('The server is now listening...\n')
+connectionSocket, addr = serverSocket.accept()
+
 
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    print('New connection from %s:%d' % (addr[0], addr[1]))
+    data = connectionSocket.recv(1024).decode()
+    if not data:
+        break
+    elif data == 'killsrv':
+        connectionSocket.close()
+        sys.exit()
+    else:
+       print(data)
+
+    connectionSocket.send(data.encode())
     
 connectionSocket.close()
