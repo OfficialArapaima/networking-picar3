03eef14122cd940a8704d9d367a779e8ac53c840
Arjun Sharma <arjun.sharma@nwciowa.edu>
2025-11-22 14:20:24 -0600

    Server Input

--- picar-server.py
+++ picar-server.py
@@ -1,5 +1,6 @@
 import sys
 from socket import *
+from picarx import Picarx
 serverPort = 12000
 
 # bind the server to the socket
@@ -10,14 +11,18 @@ serverSocket.listen(1)
 print('The server is now listening...\n')
 connectionSocket, addr = serverSocket.accept()
 
-
+px = Picarx()
 while True:
     print('New connection from %s:%d' % (addr[0], addr[1]))
     data = connectionSocket.recv(1024).decode()
+    if 'w' in data:
+        px.set_dir_servo_angle(0)
+        px.forward(80)
     if not data:
         break
     elif data == 'killsrv':
         connectionSocket.close()
+        px.stop()
         sys.exit()
     else:
        print(data)
